3924f27c112b4a2329b2b459737fd9d374616e8d
PierreDurrr <73993035+PierreDurrr@users.noreply.github.com>
2021-01-29 08:43:25 +0100

    Update update_git_scripts_schedule.sh

--- update_git_scripts_schedule.sh
+++ update_git_scripts_schedule.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 #
 ######UPDATE SCRIPTS######
-cd /scripts/000-JBOPS
+cd /_0_scripts/000-JBOPS
 git pull
-cd /scripts/000-Plex-Auto-Collections
+cd /_0_scripts/000-Plex-Auto-Collections
 git pull
-cd /scripts/000-TMDB-Collection-Data-Retriever
+cd /_0_scripts/000-TMDB-Collection-Data-Retriever
 git pull
-cd /scripts/00-Slacktee
+cd /_0_scripts/00-Slacktee
 git pull
 cd 03-ovh-dynhost
 git pull
 #
 #
 ######UPDATE CONFIG######
-cd /scripts-configs/
+cd /_0_scripts-configs/
 git pull
